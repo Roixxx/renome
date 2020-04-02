9630743a9713a68003a9c3c10df02a1bea7b1d04
Roi <vosroix@gmail.com>
2020-04-03 00:26:06 +0500

    added new slides for the header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
-// Your main script
 
-const index = 'This is a placeholder!';
+$(document).ready(function(){
+    $('.h-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        dots: false,
+        rows: 0
+    });
+});
