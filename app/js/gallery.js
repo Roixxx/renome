cba417c5ec600fb8e767f76e403456fc6846f268
Roi <vosroix@gmail.com>
2020-05-05 21:31:03 +0500

    for for the gallery

--- app/js/gallery.js
+++ app/js/gallery.js
@@ -1,8 +1,19 @@
 
 
 document.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (event.target.classList.contains('gallery__modal-close')) {
+        closeModal();
+    }
+
+    if (event.target.closest('.gallery__modal-arrow')) {
+        slideImg(event.target);
+    }
+
 
     let target = event.target.firstElementChild;
+    if (!target) return;
     if (target.classList.contains('gallery__img-link')) {
         doFullImg(target);
     }
@@ -13,12 +24,23 @@ let galleryModalHolder = document.querySelector('.gallery__modal-holder');
 let galleryModal = document.querySelector('.gallery__modal');
 
 function doFullImg(link) {
-
     body.classList.toggle('no-scroll');
     galleryModalHolder.style.display = "flex";
     galleryModal.innerHTML = `<img src="${link}" alt="fullImg">`;
-
 }
 
+function closeModal() {
+    body.classList.toggle('no-scroll');
+    galleryModalHolder.style.display = "none";
+    galleryModal.innerHTML = "";
+}
 
+function slideImg(target) {
+    if (target.dataset.dir == 'prev') {
+        
+        if () {
+            
+        }
 
+    }
+}
