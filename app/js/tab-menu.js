b7791ee98f4ede0fb80703b9b57fcd274a5ae6ad
Roi <vosroix@gmail.com>
2020-05-15 16:42:41 +0500

    added a new tab-menu script

---
+++ app/js/tab-menu.js
@@ -0,0 +1,27 @@
+
+const tabsLinksArr = Array.from(document.querySelectorAll('.tab-menu__link'));
+const tabsArr = Array.from(document.querySelectorAll('.tab-menu__section'));
+
+
+
+
+tabsLinksArr.forEach(link => link.addEventListener('click', function (e) {
+    e.preventDefault();
+    doTab(e.target);
+}));
+
+
+function doTab(target) {
+
+    tabsLinksArr.forEach(link => {link.classList.remove('active');})
+    
+    tabsArr.forEach(tab => {
+        tab.style.display = 'none';
+
+        
+        if (tab.dataset.tabMenuSection == target.dataset.tabMenuSection) {
+            tab.style.display = 'block';
+            target.classList.add('active')
+        }
+    });
+}
